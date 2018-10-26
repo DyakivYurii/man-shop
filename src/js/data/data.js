a03edf1a0927fed86af280cb52ed5343a612cb63
DyakivYurii <dyakivyurii@gmail.com>
2018-10-26 11:36:32 +0300

    Change item View

--- src/js/data/data.js
+++ src/js/data/data.js
@@ -1,7 +1,18 @@
 export const initialState = {
     'dsfs2341': {
         'name': `Cool Jaket For Real Man`,
-        'srcOfImg': `./img/item-1.jpg`,
+        'srcOfImg': {
+            'x1-xs': `img/item-1-xs-1x.jpg`,
+            'x2-xs': `img/item-1-xs-2x.jpg 2x`,
+            'x1-sm': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-sm': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-md': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-md': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-lg': `img/item-1-lg-1x.jpg 1x`,
+            'x2-lg': `img/item-1-lg-2x.jpg 2x`,
+            'x1-xl': `img/item-1-lg-1x.jpg 1x`,
+            'x2-xl': `img/item-1-lg-2x.jpg 2`
+        },
         'price': 40,
         'like': 0,
         'comment': {},
@@ -9,7 +20,18 @@ export const initialState = {
     },
     'dsfd1234': {
         'name': `Cool Shoes for Real Man`,
-        'srcOfImg': `./img/item-2.jpg`,
+        'srcOfImg': {
+            'x1-xs': `img/item-1-xs-1x.jpg`,
+            'x2-xs': `img/item-1-xs-2x.jpg 2x`,
+            'x1-sm': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-sm': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-md': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-md': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-lg': `img/item-1-lg-1x.jpg 1x`,
+            'x2-lg': `img/item-1-lg-2x.jpg 2x`,
+            'x1-xl': `img/item-1-lg-1x.jpg 1x`,
+            'x2-xl': `img/item-1-lg-2x.jpg 2`
+        },
         'price': 100,
         'like': 0,
         'comment': {},
@@ -17,7 +39,18 @@ export const initialState = {
     },
     'ds151234': {
         'name': `Bag For Man`,
-        'srcOfImg': `./img/item-1.jpg`,
+        'srcOfImg': {
+            'x1-xs': `img/item-1-xs-1x.jpg`,
+            'x2-xs': `img/item-1-xs-2x.jpg 2x`,
+            'x1-sm': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-sm': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-md': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-md': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-lg': `img/item-1-lg-1x.jpg 1x`,
+            'x2-lg': `img/item-1-lg-2x.jpg 2x`,
+            'x1-xl': `img/item-1-lg-1x.jpg 1x`,
+            'x2-xl': `img/item-1-lg-2x.jpg 2`
+        },
         'price': 130,
         'like': 0,
         'comment': {},
@@ -25,7 +58,18 @@ export const initialState = {
     },
     'ds23534': {
         'name': `Bag For Woman`,
-        'srcOfImg': `./img/item-2.jpg`,
+        'srcOfImg': {
+            'x1-xs': `img/item-1-xs-1x.jpg`,
+            'x2-xs': `img/item-1-xs-2x.jpg 2x`,
+            'x1-sm': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-sm': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-md': `img/item-1-sm-md-1x.jpg 1x`,
+            'x2-md': `img/item-1-sm-md-2x.jpg 2x`,
+            'x1-lg': `img/item-1-lg-1x.jpg 1x`,
+            'x2-lg': `img/item-1-lg-2x.jpg 2x`,
+            'x1-xl': `img/item-1-lg-1x.jpg 1x`,
+            'x2-xl': `img/item-1-lg-2x.jpg 2`
+        },
         'price': 150,
         'like': 0,
         'comment': {},
--- src/js/item/itemView.js
+++ src/js/item/itemView.js
@@ -16,9 +16,15 @@ export default class ItemView {
     render(items) {
         Object.entries(items).forEach(([id, item]) => {
             const markup = `
-            <article class="product__item col-sm-4" data-id="${id}">
+            <article class="product__item col-sm-6 col-md-6 col-lg-4" data-id="${id}">
                 <div class="product__wrapper">
-                    <img class="product__img" src="${item.srcOfImg}" alt="image of product">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="${item.srcOfImg[`x1-xl`]}, ${item.srcOfImg[`x2-xl`]}">
+                        <source media="(min-width: 992px)" srcset="${item.srcOfImg[`x1-lg`]}, ${item.srcOfImg[`x2-lg`]}">
+                        <source media="(min-width: 768px)" srcset="${item.srcOfImg[`x1-md`]}, ${item.srcOfImg[`x2-md`]}">
+                        <source media="(min-width: 576px)" srcset="${item.srcOfImg[`x1-sm`]}, ${item.srcOfImg[`x2-sm`]}">
+                        <img class="product__img" src="${item.srcOfImg[`x1-xs`]}" srcset="${item.srcOfImg[`x2-xs`]}" alt="item image">
+                    </picture>
                     <div class="product__info">
                         <h3 class="product__name">${item.name}</h3>
                         <p class="product__like"></p>
@@ -28,6 +34,11 @@ export default class ItemView {
                     <button class="basket" type="button">Add to card</button>
                 </div>
             </article>`;
+
+            
+          
+                
+       
             this.container.insertAdjacentHTML('beforeend', markup);
         });
     }
